5f5afb481007ce81428968c5660c43f6a38fad09
prince takyi <takyip28@gmail.com>
2023-08-25 20:40:40 +0000

    Use a range based approach to select words

--- src/js/grid.js
+++ src/js/grid.js
@@ -2,12 +2,33 @@ export class Grid {
   constructor() {
     this.wordSelectMode = false;
     this.selectedItems = [];
+    this.firstSelectedItem;
+    this.gridArea = null;
   }
+
+  getCellsInRange(firstLetter, currentLetter) {
+    let cellsInRange = [];
+    if (firstLetter.y === currentLetter.y) {
+      if (firstLetter.x > currentLetter.x) {
+        [currentLetter, firstLetter] = [firstLetter, currentLetter];
+      }
+      for (let i = firstLetter.x; i <= currentLetter.x; i++) {
+        cellsInRange.push(
+          this.gridArea.querySelector(
+            `td[data-x="${i}"][data-y="${currentLetter.y}"]`
+          )
+        );
+      }
+    }
+    return cellsInRange;
+  }
+
   renderGrid(gridSize, wordGrid) {
     const gridArea = document.getElementsByClassName("grid-area")[0];
     if (gridArea.lastChild) {
       gridArea.removeChild(gridArea.lastChild);
     }
+    this.gridArea = gridArea;
     const tbl = document.createElement("table");
     const tblBody = document.createElement("tbody");
     let index = 0;
@@ -35,28 +56,43 @@ export class Grid {
     // click handlers
     gridArea.addEventListener("mousedown", (event) => {
       this.wordSelectMode = true;
+      const cell = event.target;
+      cell.classList.add("selected");
+      let x = +cell.getAttribute("data-x");
+      let y = +cell.getAttribute("data-y");
+      let letter = cell.getAttribute("data-letter");
+      this.firstSelectedItem = {
+        x,
+        y,
+        letter,
+        cell,
+      };
     });
-    
+
     gridArea.addEventListener("mousemove", (event) => {
       if (this.wordSelectMode) {
         const cell = event.target;
-        cell.classList.add("selected");
-        let x = cell.getAttribute("data-x");
-        let y = cell.getAttribute("data-y");
+        // cell.classList.add("selected");
+        let x = +cell.getAttribute("data-x");
+        let y = +cell.getAttribute("data-y");
         let letter = cell.getAttribute("data-letter");
-        if (this.selectedItems.length > 0) {
-          const lastSelectedItem =
-            this.selectedItems[this.selectedItems.length - 1];
-          if (lastSelectedItem.x === x && lastSelectedItem.y === y) return;
-        }
-        this.selectedItems.push({cell, x, y, letter});
+        // if (this.selectedItems.length > 0) {
+        //   const lastSelectedItem =
+        //     this.selectedItems[this.selectedItems.length - 1];
+        //   if (lastSelectedItem.x === x && lastSelectedItem.y === y) return;
+        // }
+        // this.selectedItems.push({cell, x, y, letter});
+        this.getCellsInRange(this.firstSelectedItem, { x, y }).forEach((cell) =>
+          cell.classList.add("selected")
+        );
       }
     });
 
     gridArea.addEventListener("mouseup", (event) => {
       this.wordSelectMode = false;
-      this.selectedItems.forEach((item) => item.cell.classList.remove("selected"));
+      this.selectedItems.forEach((item) =>
+        item.cell.classList.remove("selected")
+      );
     });
   }
 }
- 
